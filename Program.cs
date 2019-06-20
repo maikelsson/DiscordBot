b191c6cc5b0c24df35ce322bf6058604f9734da3
Mikael Thornberg <mikael@Mikael-MBP.home>
2019-06-20 14:01:59 +0300

    Testing connections. Bot connected succesfully.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace DiscordBot
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            //Hide the token somewhere...
+            await _client.LoginAsync(TokenType.Bot, "NTUyNzYxNjIwNTUyNjEzODg4.XQs-Zg.i9hZLYFKVLHeJK_k0L-ii3eaAIU");
+	        await _client.StartAsync();
+
+	        // Block this task until the program is closed.
+	        await Task.Delay(-1);
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            System.Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
